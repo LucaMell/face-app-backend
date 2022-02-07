7c233e559826ddc430458debb70b66544ec56dd1
LucaMell <lm@nextagenda.dk>
2022-02-07 11:47:45 +0100

    connection string

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    host : 'postgresql-shallow-43677',
-    user : 'luca',
-    password : '',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,,
+    ssl: false,
   }
 });
 
